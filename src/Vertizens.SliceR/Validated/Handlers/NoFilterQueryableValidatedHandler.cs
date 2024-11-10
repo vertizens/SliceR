02e229f1e18c21364b2ae45862e4abe35afd6a34
wilsonshack <wilsonshack@gmail.com>
2024-11-10 12:30:02 -0600

    feat: Add NoFilterQueryableValidatedHandler Domain Projection option

--- src/Vertizens.SliceR/Validated/Handlers/NoFilterQueryableValidatedHandler.cs
+++ src/Vertizens.SliceR/Validated/Handlers/NoFilterQueryableValidatedHandler.cs
@@ -1,4 +1,6 @@
-﻿using Vertizens.SliceR.Operations;
+﻿using System.Linq.Expressions;
+using Vertizens.SliceR.Operations;
+using Vertizens.TypeMapper;
 
 namespace Vertizens.SliceR.Validated;
 public class NoFilterQueryableValidatedHandler<TEntity>(
@@ -8,5 +10,23 @@ public class NoFilterQueryableValidatedHandler<TEntity>(
     public async Task<ValidatedResult<IQueryable<TEntity>>> Handle(NoFilter request, CancellationToken cancellationToken = default)
     {
         return new ValidatedResult<IQueryable<TEntity>>(await _handler.Handle(request, cancellationToken));
+    }
+}
+
+public class NoFilterQueryableValidatedHandler<TEntity, TDomain>(
+    IHandler<NoFilter, IQueryable<TEntity>> _handler,
+    ITypeProjector<TEntity, TDomain> _typeProjector
+    ) : IValidatedHandler<NoFilter, IQueryable<TDomain>>
+    where TDomain : class, new()
+{
+    public async Task<ValidatedResult<IQueryable<TDomain>>> Handle(NoFilter request, CancellationToken cancellationToken = default)
+    {
+        var queryable = await _handler.Handle(request, cancellationToken);
+        return new ValidatedResult<IQueryable<TDomain>>(queryable.Select(Project()));
+    }
+
+    protected virtual Expression<Func<TEntity, TDomain>> Project()
+    {
+        return _typeProjector.GetProjection();
     }
 }
